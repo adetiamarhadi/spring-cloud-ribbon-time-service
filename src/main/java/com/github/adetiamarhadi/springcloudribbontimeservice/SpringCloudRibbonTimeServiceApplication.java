13127feed90299b47508359bdf4957c7a8340a17
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-17 09:45:20 +0700

    demo spring cloud ribbon

---
+++ src/main/java/com/github/adetiamarhadi/springcloudribbontimeservice/SpringCloudRibbonTimeServiceApplication.java
@@ -0,0 +1,29 @@
+package com.github.adetiamarhadi.springcloudribbontimeservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class SpringCloudRibbonTimeServiceApplication {
+
+	@Value("${server.port}")
+	int port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRibbonTimeServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String getTime() {
+		return "the current time is " + LocalDateTime.now().toString() + " (answered by service running on "+port+")";
+	}
+
+}
---
+++ src/test/java/com/github/adetiamarhadi/springcloudribbontimeservice/SpringCloudRibbonTimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springcloudribbontimeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRibbonTimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
